1f96a687a5cc9cfafc70dca49e26c3969b564966
Anonymous Applicant <anonymous@example.com>
2018-03-27 10:26:37 -0600

    add wrap-params to allow the form to pass values along the request

--- src/my_exercise/core.clj
+++ src/my_exercise/core.clj
@@ -3,16 +3,19 @@
             [compojure.route :as route]
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
             [ring.middleware.reload :refer [wrap-reload]]
+            ;Added wrap-params to allow the form to pass values along.
+            [ring.middleware.params :refer [wrap-params]]
             [my-exercise.home :as home]))
 
 (defroutes app
   (GET "/" [] home/page)
   (route/resources "/")
-  ;Created a POST route for the form to hit after submission.          
+  ;Created a POST route for the form to hit after submission.
   (POST "/search" {form-params :form-params} home/search)
   (route/not-found "Not found"))
 
 (def handler
   (-> app
       (wrap-defaults site-defaults)
+      wrap-params
       wrap-reload))
