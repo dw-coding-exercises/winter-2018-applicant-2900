8db8983156f9f50fa745965299e1ab0d86b94a32
Anonymous Applicant <anonymous@example.com>
2018-03-27 11:03:30 -0600

    refactor test suite to use a form-params function to define the params map

--- test/my_exercise/home_test.clj
+++ test/my_exercise/home_test.clj
@@ -2,10 +2,12 @@
   (:require [clojure.test :refer :all]
             [my-exercise.home :refer :all]))
 
+(def form-params {:city "New York City" :state "NY"})
+
 (deftest place-ocd-test
   (testing "Test that the Place OCD conversion works correctly"
-    (is (= "place:new_york_city" (convert-to-place-ocd {:city "New York City"})))))
+    (is (= "place:new_york_city" (convert-to-place-ocd form-params)))))
 
 (deftest state-ocd-test
   (testing "Test that the State OCD conversion works correctly"
-    (is (= "state:ny" (convert-to-state-ocd {:state "NY"})))))
+    (is (= "state:ny" (convert-to-state-ocd form-params)))))
