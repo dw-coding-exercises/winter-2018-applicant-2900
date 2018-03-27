70b94e311eb9d4ad54d3e081f7a3b2e89292f78c
Anonymous Applicant <anonymous@example.com>
2018-03-27 10:24:16 -0600

    create a POST route for the form to hit after submission.

--- src/my_exercise/core.clj
+++ src/my_exercise/core.clj
@@ -8,6 +8,8 @@
 (defroutes app
   (GET "/" [] home/page)
   (route/resources "/")
+  ;Created a POST route for the form to hit after submission.          
+  (POST "/search" {form-params :form-params} home/search)
   (route/not-found "Not found"))
 
 (def handler
