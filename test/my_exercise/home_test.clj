755a2b03d8f682d38e2a328efc43b1a9948a9824
Anonymous Applicant <anonymous@example.com>
2018-03-27 11:35:17 -0600

    create test for create-url function

--- test/my_exercise/home_test.clj
+++ test/my_exercise/home_test.clj
@@ -4,6 +4,15 @@
 
 (def form-params {:city "New York City" :state "NY"})
 
+(def test-url
+  "https://api.turbovote.org/elections/upcoming?district-divisions=ocd-division/country:us/state:ny,ocd-division/country:us/state:ny/place:new_york_city")
+
+(def test-place-ocd
+  "place:new_york_city")
+
+(def test-state-ocd
+  "state:ny")
+
 (deftest place-ocd-test
   (testing "Test that the Place OCD conversion works correctly"
     (is (= "place:new_york_city" (convert-to-place-ocd form-params)))))
@@ -11,3 +20,7 @@
 (deftest state-ocd-test
   (testing "Test that the State OCD conversion works correctly"
     (is (= "state:ny" (convert-to-state-ocd form-params)))))
+
+(deftest url-test
+  (testing "Test that the url created is correct"
+    (is (= test-url (create-url test-place-ocd test-state-ocd)))))
